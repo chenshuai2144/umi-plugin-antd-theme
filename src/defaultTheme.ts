32cc72d54a122e7c1251ce02ee94b29490b681be
chenshuai2144 <qixian.cs@outlook.com>
2021-11-16 16:08:54 +0800

    support morethemeTemp

--- src/defaultTheme.ts
+++ src/defaultTheme.ts
@@ -3,114 +3,114 @@
 export default {
   theme: [
     {
-      key: 'dark',
-      fileName: 'dark.css',
-      theme: 'dark',
+      key: "dark",
+      fileName: "dark.css",
+      theme: "dark",
     },
     {
-      key: 'dust',
-      fileName: 'dust.css',
+      key: "dust",
+      fileName: "dust.css",
       modifyVars: {
-        '@primary-color': '#F5222D',
+        "@primary-color": "#F5222D",
       },
     },
     {
-      key: 'volcano',
-      fileName: 'volcano.css',
+      key: "volcano",
+      fileName: "volcano.css",
       modifyVars: {
-        '@primary-color': '#FA541C',
+        "@primary-color": "#FA541C",
       },
     },
     {
-      key: 'sunset',
-      fileName: 'sunset.css',
+      key: "sunset",
+      fileName: "sunset.css",
       modifyVars: {
-        '@primary-color': '#FAAD14',
+        "@primary-color": "#FAAD14",
       },
     },
     {
-      key: 'cyan',
-      fileName: 'cyan.css',
+      key: "cyan",
+      fileName: "cyan.css",
       modifyVars: {
-        '@primary-color': '#13C2C2',
+        "@primary-color": "#13C2C2",
       },
     },
     {
-      key: 'green',
-      fileName: 'green.css',
+      key: "green",
+      fileName: "green.css",
       modifyVars: {
-        '@primary-color': '#52C41A',
+        "@primary-color": "#52C41A",
       },
     },
     {
-      key: 'geekblue',
-      fileName: 'geekblue.css',
+      key: "geekblue",
+      fileName: "geekblue.css",
       modifyVars: {
-        '@primary-color': '#2F54EB',
+        "@primary-color": "#2F54EB",
       },
     },
     {
-      key: 'purple',
-      fileName: 'purple.css',
+      key: "purple",
+      fileName: "purple.css",
       modifyVars: {
-        '@primary-color': '#722ED1',
+        "@primary-color": "#722ED1",
       },
     },
 
     {
-      key: 'dust',
-      theme: 'dark',
-      fileName: 'dark-dust.css',
+      key: "dust",
+      theme: "dark",
+      fileName: "dark-dust.css",
       modifyVars: {
-        '@primary-color': '#F5222D',
+        "@primary-color": "#F5222D",
       },
     },
     {
-      key: 'volcano',
-      theme: 'dark',
-      fileName: 'dark-volcano.css',
+      key: "volcano",
+      theme: "dark",
+      fileName: "dark-volcano.css",
       modifyVars: {
-        '@primary-color': '#FA541C',
+        "@primary-color": "#FA541C",
       },
     },
     {
-      key: 'sunset',
-      theme: 'dark',
-      fileName: 'dark-sunset.css',
+      key: "sunset",
+      theme: "dark",
+      fileName: "dark-sunset.css",
       modifyVars: {
-        '@primary-color': '#FAAD14',
+        "@primary-color": "#FAAD14",
       },
     },
     {
-      key: 'cyan',
-      theme: 'dark',
-      fileName: 'dark-cyan.css',
+      key: "cyan",
+      theme: "dark",
+      fileName: "dark-cyan.css",
       modifyVars: {
-        '@primary-color': '#13C2C2',
+        "@primary-color": "#13C2C2",
       },
     },
     {
-      key: 'green',
-      theme: 'dark',
-      fileName: 'dark-green.css',
+      key: "green",
+      theme: "dark",
+      fileName: "dark-green.css",
       modifyVars: {
-        '@primary-color': '#52C41A',
+        "@primary-color": "#52C41A",
       },
     },
     {
-      key: 'geekblue',
-      theme: 'dark',
-      fileName: 'dark-geekblue.css',
+      key: "geekblue",
+      theme: "dark",
+      fileName: "dark-geekblue.css",
       modifyVars: {
-        '@primary-color': '#2F54EB',
+        "@primary-color": "#2F54EB",
       },
     },
     {
-      key: 'purple',
-      theme: 'dark',
-      fileName: 'dark-purple.css',
+      key: "purple",
+      theme: "dark",
+      fileName: "dark-purple.css",
       modifyVars: {
-        '@primary-color': '#722ED1',
+        "@primary-color": "#722ED1",
       },
     },
   ],
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 /** @format */
 
 // - https://umijs.org/plugin/develop.html
-import { IApi } from 'umi';
-import { join } from 'path';
-import serveStatic from 'serve-static';
-import rimraf from 'rimraf';
-import { existsSync, mkdirSync } from 'fs';
-import defaultTheme from './defaultTheme';
+import { IApi } from "umi";
+import { join } from "path";
+import serveStatic from "serve-static";
+import rimraf from "rimraf";
+import { existsSync, mkdirSync } from "fs";
+import defaultTheme from "./defaultTheme";
 
-const buildCss = require('antd-pro-merge-less');
-const winPath = require('slash2');
+const buildCss = require("antd-pro-merge-less");
+const winPath = require("slash2");
 
 interface themeConfig {
   theme?: string;
@@ -27,23 +27,23 @@ export default function (api: IApi) {
             resourcePath: string;
           },
           _: string,
-          localName: string,
+          localName: string
         ) => {
           if (
-            context.resourcePath.includes('node_modules') ||
-            context.resourcePath.includes('ant.design.pro.less') ||
-            context.resourcePath.includes('global.less')
+            context.resourcePath.includes("node_modules") ||
+            context.resourcePath.includes("ant.design.pro.less") ||
+            context.resourcePath.includes("global.less")
           ) {
             return localName;
           }
           const match = context.resourcePath.match(/src(.*)/);
           if (match && match[1]) {
-            const antdProPath = match[1].replace('.less', '');
+            const antdProPath = match[1].replace(".less", "");
             const arr = winPath(antdProPath)
-              .split('/')
-              .map((a: string) => a.replace(/([A-Z])/g, '-$1'))
+              .split("/")
+              .map((a: string) => a.replace(/([A-Z])/g, "-$1"))
               .map((a: string) => a.toLowerCase());
-            return `antd-pro${arr.join('-')}-${localName}`.replace(/--/g, '-');
+            return `antd-pro${arr.join("-")}-${localName}`.replace(/--/g, "-");
           }
           return localName;
         },
@@ -58,13 +58,19 @@ export default function (api: IApi) {
   } = defaultTheme;
 
   // 从固定的路径去读取配置，而不是从 config 中读取
-  const themeConfigPath = winPath(join(api.paths.cwd, 'config/theme.config.json'));
+  const themeConfigPath = winPath(
+    join(api.paths.cwd, "config/theme.config.json")
+  );
   if (existsSync(themeConfigPath)) {
     options = require(themeConfigPath);
   }
   const { cwd, absOutputPath, absNodeModulesPath } = api.paths;
   const outputPath = absOutputPath;
-  const themeTemp = winPath(join(__dirname, '../../.plugin-theme'));
+  let themeTemp = winPath(join(__dirname, "../../.plugin-theme"));
+
+  if (existsSync(winPath(join(absNodeModulesPath, ".plugin-theme")))) {
+    themeTemp = winPath(join(absNodeModulesPath, ".plugin-theme"));
+  }
 
   // 增加中间件
   api.addMiddewares(() => {
@@ -74,7 +80,9 @@ export default function (api: IApi) {
   // 增加一个对象，用于 layout 的配合
   api.addHTMLHeadScripts(() => [
     {
-      content: `window.umi_plugin_ant_themeVar = ${JSON.stringify(options.theme)}`,
+      content: `window.umi_plugin_ant_themeVar = ${JSON.stringify(
+        options.theme
+      )}`,
     },
   ]);
 
@@ -83,13 +91,13 @@ export default function (api: IApi) {
     if (err) {
       return;
     }
-    api.logger.info('💄  build theme');
+    api.logger.info("💄  build theme");
 
     try {
-      if (existsSync(winPath(join(outputPath, 'theme')))) {
-        rimraf.sync(winPath(join(outputPath, 'theme')));
+      if (existsSync(winPath(join(outputPath, "theme")))) {
+        rimraf.sync(winPath(join(outputPath, "theme")));
       }
-      mkdirSync(winPath(join(outputPath, 'theme')));
+      mkdirSync(winPath(join(outputPath, "theme")));
     } catch (error) {
       // console.log(error);
     }
@@ -98,15 +106,15 @@ export default function (api: IApi) {
       cwd,
       options.theme.map((theme) => ({
         ...theme,
-        fileName: winPath(join(outputPath, 'theme', theme.fileName)),
+        fileName: winPath(join(outputPath, "theme", theme.fileName)),
       })),
       {
         min: true,
         ...options,
-      },
+      }
     )
       .then(() => {
-        api.logger.log('🎊  build theme success');
+        api.logger.log("🎊  build theme success");
       })
       .catch((e) => {
         console.log(e);
@@ -115,20 +123,20 @@ export default function (api: IApi) {
 
   // dev 之后
   api.onDevCompileDone(() => {
-    api.logger.info('cache in :' + themeTemp);
-    api.logger.info('💄  build theme');
+    api.logger.info("cache in :" + themeTemp);
+    api.logger.info("💄  build theme");
     // 建立相关的临时文件夹
     try {
       if (existsSync(themeTemp)) {
         rimraf.sync(themeTemp);
       }
-      if (existsSync(winPath(join(themeTemp, 'theme')))) {
-        rimraf.sync(winPath(join(themeTemp, 'theme')));
+      if (existsSync(winPath(join(themeTemp, "theme")))) {
+        rimraf.sync(winPath(join(themeTemp, "theme")));
       }
 
       mkdirSync(themeTemp);
 
-      mkdirSync(winPath(join(themeTemp, 'theme')));
+      mkdirSync(winPath(join(themeTemp, "theme")));
     } catch (error) {
       // console.log(error);
     }
@@ -137,14 +145,14 @@ export default function (api: IApi) {
       cwd,
       options.theme.map((theme) => ({
         ...theme,
-        fileName: winPath(join(themeTemp, 'theme', theme.fileName)),
+        fileName: winPath(join(themeTemp, "theme", theme.fileName)),
       })),
       {
         ...options,
-      },
+      }
     )
       .then(() => {
-        api.logger.log('🎊  build theme success');
+        api.logger.log("🎊  build theme success");
       })
       .catch((e) => {
         console.log(e);
